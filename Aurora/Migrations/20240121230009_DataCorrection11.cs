af7008ff5aec36c4bd81f488d94fa8826f48316c
M4tth3wG <90974216+M4tth3wG@users.noreply.github.com>
2024-01-22 00:00:40 +0100

    Added db migration

---
+++ Aurora/Migrations/20240121230009_DataCorrection11.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aurora.Migrations
+{
+    public partial class DataCorrection11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OpisKierunku",
+                table: "KierunkiStudiow",
+                type: "nvarchar(1023)",
+                maxLength: 1023,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1023)",
+                oldMaxLength: 1023);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OpisKierunku",
+                table: "KierunkiStudiow",
+                type: "nvarchar(1023)",
+                maxLength: 1023,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1023)",
+                oldMaxLength: 1023,
+                oldNullable: true);
+        }
+    }
+}
