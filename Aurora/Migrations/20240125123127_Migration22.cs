54fac5017e32bfd040d5b2a57a8d11ade2670549
M4tth3wG <90974216+M4tth3wG@users.noreply.github.com>
2024-01-25 13:32:20 +0100

    Added Migration22

---
+++ Aurora/Migrations/20240125123127_Migration22.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aurora.Migrations
+{
+    public partial class Migration22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AplikacjeRekrutacyjne",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "TuraRekrutacjiID",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "AplikacjeRekrutacyjne",
+                keyColumn: "ID",
+                keyValue: 11,
+                column: "TuraRekrutacjiID",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "AplikacjeRekrutacyjne",
+                keyColumn: "ID",
+                keyValue: 12,
+                column: "TuraRekrutacjiID",
+                value: 5);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AplikacjeRekrutacyjne",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "TuraRekrutacjiID",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "AplikacjeRekrutacyjne",
+                keyColumn: "ID",
+                keyValue: 11,
+                column: "TuraRekrutacjiID",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "AplikacjeRekrutacyjne",
+                keyColumn: "ID",
+                keyValue: 12,
+                column: "TuraRekrutacjiID",
+                value: 3);
+        }
+    }
+}
